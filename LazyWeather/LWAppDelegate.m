8ab28927d23ae697dd3d7d080506056dab784259
Arthur Pan <arthurpan24@gmail.com>
2015-10-16 12:59:51 -0700

    imported WeatherUpdateManager to AppDelegate

--- LazyWeather/LWAppDelegate.m
+++ LazyWeather/LWAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "LWAppDelegate.h"
+#import "LWWeatherUpdateManager.h"
 
 @interface LWAppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [LWWeatherUpdateManager sharedManager];
     return YES;
 }
 
